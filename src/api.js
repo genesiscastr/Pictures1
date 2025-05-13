834f6449114c6c7d48d607ab4512e530df57aa05
genesiscastr <genesis.cstroz@gmail.com>
2025-05-13 14:08:03 -0700

    se modificaron

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 
 const searchImages = async(term) => {
     const url = 'https://api.unsplash.com/search/photos/?client_id=BzYrP5E9GofO89RKWhkRAwCc2Bn1S6KNjZxiZg_kvno'
