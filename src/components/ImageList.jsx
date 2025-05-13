917b343a157decb047dadf370a95d6db165934b9
genesiscastr <genesis.cstroz@gmail.com>
2025-05-13 14:24:53 -0700

    se modificaron.

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,14 +1,13 @@
 import ImageShow from "./ImageShow"
-import './ImageList.css' 
+import './ImageList.css'
 
 const ImageList = ({images}) => {
-    const renderImage = images.map((images) =>{
-        return <ImageShow key={image.id} image={image} />
-    
-})
-return (
+  const renderImage = images.map((image) => {
+    return <ImageShow key={image.id} image={image} />
+  })
+  return (
     <div className="image-list">
-        {renderImage}
+      {renderImage}
     </div>
   )
 }
