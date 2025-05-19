b3b50aa7d0777f13b185d3e7a7a60c925a5d5542
genesiscastr <genesis.cstroz@gmail.com>
2025-05-19 14:17:45 -0700

    se guardo.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=iIkagcdIwjN4VnxCObuhD1g5UNpbQvOk7U9HbTghD9k'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=BzYrP5E9GofO89RKWhkRAwCc2Bn1S6KNjZxiZg_kvno'
 
     const response = await axios.get(url, {
         params: {
@@ -9,7 +9,7 @@ const searchImages = async(term) => {
         }
     })
 
-    conmsole.log(response)
+    console.log(response)
     return response.data.results
 }
 
