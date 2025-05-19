49dd78f34bd7fdaaf1e1adad64166b526dac2829
genesiscastr <genesis.cstroz@gmail.com>
2025-05-19 14:36:40 -0700

    se guardo.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
 import searchImages from "./api"
 
-function App() {
-  //searchImages('cars')
-  const [images, setImages] = useState([])
-
-  const handleSubmit = async(term) => {
-    console.log('Usted esta buscando con: ', term)
-    const result = await searchImages(term)
-    setImages(result)
-  }
   return (
     <>
       <h1>Pictures App</h1>
@@ -19,7 +10,7 @@ function App() {
       <ImageList images={images}/>
     </>
   )
-}
+
 
 export default App
 
